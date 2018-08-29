89f7aa6e60a1d2bbdfbd8db87f59bca8876a2b51
mirana2 <miratsoah@gmail.com>
2018-08-29 13:16:39 +0300

    avec devise

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Item.create(title: "c1.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c2.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c3.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c4.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c5.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c6.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c7.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c8.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c9.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c10.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c11.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c12.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c13.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c14.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c15.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c16.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c17.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c18.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c19.jpg", description: "", price: "", image_url: "")
+Item.create(title: "c20.jpg", description: "", price: "", image_url: "")
